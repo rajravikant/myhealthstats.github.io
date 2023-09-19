f98a2d3f809c2526dea20d0d9c623e7416fbe896
GeekyRavi <108944355+rajravikant@users.noreply.github.com>
2023-09-20 02:15:20 +0530

    20 sep update
    major changes in index.html and added login page

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,18 @@
 const nav_header = document.querySelector("header");
-const mobile_nav = document.querySelectorAll("mobile-nav-icon");
+const mobile_nav = document.querySelector(".mobile-nav-bar");
 const nav_bar_links = document.querySelectorAll("navbar-links");
 const btn_lrn = document.querySelector(".btn-learn");
 
-
-
-
 const redirectToMyAccount = () => {
-    window.open("../pages/myaccount.html");
+  window.open("../pages/myaccount.html");
 };
 
 const toggleNavbar = () => {
-    nav_header.classList.toggle("active");
+  nav_header.classList.toggle("active");
+  
 };
 
+mobile_nav.addEventListener("click",()=> toggleNavbar());
 btn_lrn.addEventListener("click", () => redirectToMyAccount());
 
 //sticky navbar
@@ -37,6 +36,32 @@ const observer = new IntersectionObserver(
 observer.observe(section_info);
 //end sticky nav bar
 
+var typed = new Typed("#element", {
+  strings: [
+    "FIT",
+    "HEALTHY",
+    "CONSISTENT",
+    "DEDICATED",
+    "FOCUSED",
+    "PASSIONATE",
+    "FIT"
+  ],
+  typeSpeed: 80,
+});
+
+var typed = new Typed("#element2", {
+  strings: [
+    "One hour of strength training can burn upto 300 to 400 calories",
+    "Muscle Memory helps you to recover quickly from a break",
+    "To loose weight adapt caloric defecet",
+    "Avoid processed foods",
+    "Increase Your Protein",
+    "Always Incorporate Strength Training",
+    "Take recovery days",
+    "Track your workout schedule",
+  ],
+  typeSpeed: 30,
+});
 
 
 
